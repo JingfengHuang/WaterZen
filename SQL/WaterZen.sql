2dd834cdbb68055e55d04be1cf72517569e4c7e9
JingfengHuang <huangjingfeng970106@gmail.com>
2021-08-17 16:28:12 +0800

    Create WaterZen.sql
    加入了自建的db

---
+++ SQL/WaterZen.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 17, 2021 at 10:27 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `WaterZen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `government`
+--
+
+CREATE TABLE `government` (
+  `id` int(11) NOT NULL,
+  `officialName` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `managerName` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phoneNum` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merchant`
+--
+
+CREATE TABLE `merchant` (
+  `id` int(11) NOT NULL,
+  `brand` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `businessCategory` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postcode` int(11) NOT NULL,
+  `managerName` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phoneNum` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nickname` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` char(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatarPath` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `government`
+--
+ALTER TABLE `government`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `merchant`
+--
+ALTER TABLE `merchant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `USER EMAIL` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `government`
+--
+ALTER TABLE `government`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `government`
+--
+ALTER TABLE `government`
+  ADD CONSTRAINT `FK_UserID` FOREIGN KEY (`id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `merchant`
+--
+ALTER TABLE `merchant`
+  ADD CONSTRAINT `FK_MerchantID` FOREIGN KEY (`id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
