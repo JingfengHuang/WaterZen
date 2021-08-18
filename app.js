db6246ec3d6350674b54c929ad448521661dc664
whitneytwn <61786514+whitneytwn@users.noreply.github.com>
2021-08-18 21:01:39 +0800

    Adding configs to app.js
    + body-parser(for json) config
    + mysql module (not connected with db yet)
    + dotenv (condig not finished)
    + public folder for css files

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
+// haven't decide on using handle bars or ejs
+// const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// Parse application/json
+app.use(express.json());
+
+// Static files
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
