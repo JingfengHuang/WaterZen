7494cffb1d27df47ebcef0b0cca3903c7842e190
whitneytwn <61786514+whitneytwn@users.noreply.github.com>
2021-08-19 15:23:01 +0800

    database insert and test
    test test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,35 @@
 var router = require('express').Router();
 const indexController = require('../controllers/indexController');
+const mysql = require('mysql');
+// Create connection pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
+});
 
 router.get('/', function(req, res) {
-    res.render('index');
+    pool.getConnection((err, connection) => {
+        if (err) throw err; //not connected
+        console.log('connected as ID ' + connection.threadId);
+    
+        // Use the connection
+        connection.query('SELECT * FROM user', (err, rows) => {
+            // when connection done, release it
+            connection.release();
+    
+            if (!err) {
+                let removedUser = req.query.removed;
+                res.render('index', {rows, removedUser});
+            } else {
+                console.log(err);
+            }
+        });
+    });
 });
+
+
 
 router.get('/login', function(req, res) {
     res.render('login');
