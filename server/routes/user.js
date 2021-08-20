6e09d59bae5d27d3567554a87d2cdd0cc102e6cc
whitneytwn <61786514+whitneytwn@users.noreply.github.com>
2021-08-20 11:58:09 +0800

    login improved
    + comments and pop up message while user input wrong password

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,27 +11,36 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// Homepage
 router.get('/', function (req, res) {
-
-    // Use the connection
     res.render('index');
-
 });
 
+// Direct to Login Page
 router.get('/login', function (req, res) {
     res.render('login');
 });
 
+// Direct to registration page
 router.get('/registration', function (req, res) {
     res.render('registration');
 });
 
+// get user input from registraion page and insert into db
 router.post('/registration/validation', function (req, res) {
+    // get user input
     const {useremail, nickname} = req.body;
-    const password = bcrypt.hashSync(req.body.password, 10);
-    const avatarPath = "default";
-    connection.query('INSERT INTO user SET email = ?, nickname = ?, password = ?, avatarPath = ?', [useremail, nickname, password, avatarPath], (err, rows) => {
 
+    // hash password
+    const password = bcrypt.hashSync(req.body.password, 10);
+
+    // set avatar path as default for now
+    const avatarPath = "default";
+
+    // insert into database
+    connection.query('INSERT INTO user SET email = ?, nickname = ?, password = ?, avatarPath = ?', [useremail, nickname, password, avatarPath], (err, rows) => {
+        
+        // if success refrech registration page
         if (!err) {
             res.render('registration');
         } else {
@@ -40,21 +49,28 @@ router.post('/registration/validation', function (req, res) {
     });
 });
 
+// check user login
 router.post('/login/checklogin', function (req, res) {
+    // get user input
     const {useremail, password} = req.body;
+
+    // query user existence
     connection.query('SELECT * FROM user WHERE email = ?', [useremail], (err, rows) => {
+
+        // if db match user email
         if (!err) {
-            
+            // get db-stored password
             let hashedpassword = rows[0].password;
-            bcrypt.compare(password, hashedpassword, function(error, res) {
-                if (!error) {
-                    console.log(res); // true
-                } else {
-                    console.log(error);
-                }
-                
-            });
-            res.render('index');
+
+            //compare user input password and db-stored password
+            var matched = bcrypt.compareSync(password, hashedpassword);
+            
+            // if matched redirect to home page, if not pop alert
+            if (matched) {
+                res.render('index');
+            } else {
+                res.render('login', {rows, alert: `Incorrect email or password`});
+            }
         } else {
             console.log(err);
         }
