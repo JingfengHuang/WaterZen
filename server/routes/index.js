0b1e4009a3cb8bd3ab807572cd6f5ba0a728816a
whitneytwn <61786514+whitneytwn@users.noreply.github.com>
2021-08-20 10:06:28 +0800

    Simplify db connection pool
    as titled

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,37 +1,34 @@
 var router = require('express').Router();
 const indexController = require('../controllers/indexController');
 const mysql = require('mysql');
+
 // Create connection pool
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME
 });
 
-router.get('/', function(req, res) {
-    pool.getConnection((err, connection) => {
-        if (err) throw err; //not connected
-        console.log('connected as ID ' + connection.threadId);
-    
-        // Use the connection
-        connection.query('SELECT * FROM user', (err, rows) => {
-            // when connection done, release it
-            connection.release();
-    
-            if (!err) {
-                let removedUser = req.query.removed;
-                res.render('index', {rows, removedUser});
-            } else {
-                console.log(err);
-            }
-        });
+router.get('/', function (req, res) {
+
+    // Use the connection
+    connection.query('SELECT * FROM user', (err, rows) => {
+        // when connection done, release it
+
+        if (!err) {
+            let removedUser = req.query.removed;
+            res.render('index', { rows, removedUser });
+        } else {
+            console.log(err);
+        }
     });
+
 });
 
 
 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
     res.render('login');
 });
 
