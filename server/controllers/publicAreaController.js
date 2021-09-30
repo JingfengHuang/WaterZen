93d59145161e3014206ef5d9ccbe6c2e7bd0a059
JingfengHuang <huangjingfeng970106@gmail.com>
2021-09-30 21:02:19 +0800

    Update public area
    1. Now the data is from the database.
    2. Animation was deleted because loading time is very short.
    3. Now the new basic backend logic is finished.

--- server/controllers/publicAreaController.js
+++ server/controllers/publicAreaController.js
@@ -1,6 +1,10 @@
 /** Imports */
 const mysql = require('mysql');
 
+//Variables
+let results = null;
+let selected = null;
+
 /** Create connection pool */
 const pool = mysql.createPool({
     connectionLimit : 100,
@@ -17,16 +21,55 @@ exports.view = (req, res) => {
     if (req.session.login) {
         pool.getConnection((err, connection) => {
             if(err) throw err; //not connected
-            const today = new Date();
-            console.log(`Connect as ID ${connection.threadId} at ${today}`);
 
             connection.query('SELECT * FROM user WHERE email = ?', [userEmail], (err, rows) => {
                 if (!err) {
-                    res.render('publicArea', {login: true, pageTitle: pageTitle, nickname: rows[0].nickname, avatar: rows[0].avatarPath});
+                    res.render('publicArea', {login: true, pageTitle: pageTitle, nickname: rows[0].nickname, avatar: rows[0].avatarPath, results:results, selected: selected, alert:req.flash('nullState')});
                 }
             });
         });
     } else {
-        res.render('publicArea', {login: false, pageTitle: pageTitle});
+        res.render('publicArea', {login: false, pageTitle: pageTitle, results:results, selected: selected, alert:req.flash('nullState')});
     }
+}
+
+exports.searchState = (req, res) => {
+    let state = req.body.state;
+    let orderBy = req.body.orderBy;
+    selected = state;
+
+    if (state == "null") {
+        req.flash('nullState', 'Please select a valid choice!');
+        return res.redirect('/publicArea/clear');
+    }
+
+    if (orderBy == "null") {
+        pool.getConnection((err, connection) => {
+            if(err) throw err; //not connected
+            connection.query('SELECT * FROM qualityData WHERE state = ?', [state], (err, rows) => {
+                if (!err) {
+                    results = rows;
+                    res.redirect('/publicArea');
+                }
+            });
+        });
+    } else if (orderBy == "city") {
+        pool.getConnection((err, connection) => {
+            if(err) throw err; //not connected
+            connection.query('SELECT * FROM qualityData WHERE state = ? ORDER BY city ASC', [state], (err, rows) => {
+                if (!err) {
+                    results = rows;
+                    res.redirect('/publicArea');
+                }
+            });
+        });
+    } else if (orderBy == "quality") {
+        return res.redirect('/publicArea/clear');
+    }
+}
+
+exports.clear = (req, res) => {
+    selected = null;
+    results = null;
+    res.redirect('/publicArea');
 }
--- server/routes/publicArea.js
+++ server/routes/publicArea.js
@@ -6,5 +6,7 @@ const publicAreaController = require('../controllers/publicAreaController');
 
 // Direct to public area page
 router.get('/publicArea', publicAreaController.view);
+router.post('/publicArea/searchState', publicAreaController.searchState);
+router.get('/publicArea/clear', publicAreaController.clear);
 
 module.exports = router;
